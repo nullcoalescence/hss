dd67b864f41695f36d64953fcfc7b68640ac6f1b
ben <{ID}.{username}@users.noreply.github.com>
2023-09-27 22:29:00 -0500

    create DbContext, add to DI, and create user model

---
+++ hss-api/Db/SupperContext.cs
@@ -0,0 +1,28 @@
+﻿using hss_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace hss_api.Db
+{
+    public class SupperContext : DbContext
+    {
+        private readonly string dbName = "supper.db";
+
+        public DbSet<User> Users { get; set; }
+
+        public string DbPath { get; }
+
+        public SupperContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+
+            DbPath = Path.Join(path, dbName);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
--- hss-api/Program.cs
+++ hss-api/Program.cs
@@ -1,3 +1,5 @@
+using hss_api.Db;
+
 namespace hss_api
 {
     public class Program
@@ -12,6 +14,9 @@ namespace hss_api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Db
+            builder.Services.AddDbContext<SupperContext>();
 
             var app = builder.Build();
 
